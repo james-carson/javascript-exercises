96dff7c6d831dea77d661f917e28fd97dbf13849
carsoninho <james.87.carson@gmail.com>
2024-05-23 11:39:31 +0200

    Finish exercise 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  const fahrToCels = ((fahr - 32) * (5 / 9));
+  return parseFloat(fahrToCels.toFixed(1));
+  // console.log(fahrToCels.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  const celsToFahr = ((cels * (9/5)) + 32);
+  return parseFloat(celsToFahr.toFixed(1));
+  // console.log(celsToFahr.toFixed(1))
 };
+
+// convertToCelsius(32)
 
 // Do not edit below this line
 module.exports = {
