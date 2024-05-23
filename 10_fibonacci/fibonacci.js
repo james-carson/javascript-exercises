bba92699151274f82f784eceddf7b71c9666d29e
carsoninho <james.87.carson@gmail.com>
2024-05-23 18:35:55 +0200

    Finished Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    
+    fibNum = parseInt(input)
 
-};
+    let n1 = 0,
+        n2 = 1,
+        newNum;
+
+    if (fibNum < 0){
+        newNum = "OOPS";
+    } else if (fibNum === 0){
+        newNum = 0
+    } else if (fibNum === 1){
+        newNum = 1
+    } else {
+        for (i = 2; i <= fibNum; i++) {
+            newNum = n1 + n2;
+            n1 = n2;
+            n2 = newNum;
+        };
+    }
+    return newNum;   
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
