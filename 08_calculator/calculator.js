70b7cb5cc0fbb0af92038ca68f4b7d2a49db3d34
carsoninho <james.87.carson@gmail.com>
2024-05-23 16:11:52 +0200

    Finished Exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(add1, add2) {
+	return (add1 + add2)
 };
 
-const subtract = function() {
-	
+const subtract = function(sub1, sub2) {
+	return (sub1 - sub2)
 };
 
-const sum = function() {
-	
+const sum = function(sum) {
+	let total = 0;
+  for (let i=0; i < sum.length; i++) {
+    total += sum[i];
+  }
+  return total;
+}
+
+const multiply = function(mult) {
+  let total = 1;
+
+  for (let i = 0; i < mult.length; i++) {
+    total *= mult[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const power = function(number, power) {
+	let sum = 1
+  for (i = 0; i < power; i++) {
+    sum *= number
+  }
+  return sum
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(factor) {
+	let sum = 1
+  if (factor > 0){
+    for (i = 0; i < factor; i++) {
+      sum *= (i + 1)
+    }
+    return sum
+  } else {
+    return 1
+  }
 };
 
 // Do not edit below this line
