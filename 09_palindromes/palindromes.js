814e5021c5462dfd88b9c33046c5f614f928153c
carsoninho <james.87.carson@gmail.com>
2024-05-23 16:36:46 +0200

    Finished Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    
+    function reverseString(str) {
+        return str.split("").reverse().join("");
+    }
+    
+    let forward = input.toLowerCase();
+
+    forwardFormatted = forward.replace(/[^a-zA-Z0-9]/g, '');
+
+    let backwards = reverseString(forwardFormatted);
+    
+    if (forwardFormatted === backwards) {
+        return true
+    } else {
+        return false
+    };
 
 };
 
